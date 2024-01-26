2969522901c3ff8ff590b515cdae0c0419d58549
almeidas-tatiane <almeidas.tatiane@gmail.com>
2024-01-26 15:02:25 -0300

    Adding Users class

--- src/gatling/scala/users/objects/Users.scala
+++ src/gatling/scala/users/objects/Users.scala
@@ -1,5 +1,39 @@
 package users.objects
 
+import io.gatling.core.Predef.{StringBody, exec}
+import io.gatling.http.Predef._
+import utils.UrlProperties
+import io.gatling.core.Predef._
+import io.gatling.core.structure.ChainBuilder
 object Users {
+
+  /* ----- TEST DATA ----- */
+  private val testDataDir = "csv/users/"
+  val loginData = csv(testDataDir + "login.csv").queue
+
+  /* ----- HEADERS ----- */
+  val sentHeadersLogin = Map(
+    "Content-Type" -> "application/json",
+    "X-Dynatrace-Test" -> "VU=$VU;SI=GATLING;TSN=LOGIN;LSN=$LSN;LTN=$LTN;PC=$PC"
+  )
+
+  /* ----- REQUESTS BODY ----- */
+  val LoginBody =
+    """{
+      |      "username": "${username}" ,
+      |      "password": "${password}"
+      |    }""".stripMargin
+
+  /* ----- REQUESTS ----- */
+  def login: ChainBuilder = {
+    feed(loginData)
+    .exec(
+      http("Login -> /auth/token/login/")
+        .post(UrlProperties.getUrlByKey("api") + "/auth/token/login/")
+        .headers(sentHeadersLogin)
+        .body(StringBody(LoginBody)).asJson
+        .check(jsonPath("$.access").saveAs("access_token"))
+    )
+  }
 
 }
