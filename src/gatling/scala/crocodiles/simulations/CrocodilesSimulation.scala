69ac60a95657e09f5817a81a0cdade291bdff876
almeidas-tatiane <almeidas.tatiane@gmail.com>
2024-01-26 13:10:01 -0300

    Adding Load test

--- src/gatling/scala/crocodiles/simulations/CrocodilesSimulation.scala
+++ src/gatling/scala/crocodiles/simulations/CrocodilesSimulation.scala
@@ -7,15 +7,44 @@ import scala.concurrent.duration.DurationInt
 
 class CrocodilesSimulation extends Simulation {
 
+  /* ----- VARIABLES ----- */
   val httpConfiguration = http
-    .acceptHeader("application/json")
+  val users = System.getProperty("users", "15").toInt
+  val rampDuration = System.getProperty("rampDuration", "120").toInt.seconds
+  val holdDuration = System.getProperty("holdDuration", "10").toInt.minutes
+  val maxDuration = System.getProperty("maxDuration", "130").toInt.minutes
 
   def scnGetAllCrocodiles = scenario("Get All Crocodiles")
-    .repeat(5) {
-      exec(Crocodiles.getAllCrocodiles)
-    }
+      .exec(Crocodiles.getAllCrocodiles)
 
-  setUp(scnGetAllCrocodiles.inject(
-    atOnceUsers(1))).protocols(httpConfiguration)
+  /* ----- VALIDATE TEST ----- */
+//  setUp(scnGetAllCrocodiles.inject(
+//    atOnceUsers(1))).protocols(httpConfiguration)
+
+  /* ----- LOAD TEST ----- */
+  setUp(
+    scnGetAllCrocodiles.inject(
+      rampUsers(users).during(rampDuration),
+      nothingFor(holdDuration),
+      rampUsers(users).during(rampDuration),
+      nothingFor(holdDuration),
+      rampUsers(users).during(rampDuration),
+      nothingFor(holdDuration),
+      rampUsers(users).during(rampDuration),
+      nothingFor(holdDuration),
+      rampUsers(users).during(rampDuration),
+      nothingFor(holdDuration),
+      rampUsers(users).during(rampDuration),
+      nothingFor(holdDuration),
+      rampUsers(users).during(rampDuration),
+      nothingFor(holdDuration),
+      rampUsers(users).during(rampDuration),
+      nothingFor(holdDuration),
+      rampUsers(users).during(rampDuration),
+      nothingFor(holdDuration),
+      rampUsers(users).during(rampDuration),
+      nothingFor(holdDuration * 2)
+    ).protocols(httpConfiguration)).maxDuration(maxDuration))
+
 
 }
