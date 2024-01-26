ac37ca530ec5173efa2f8916a6b4dad8875b2019
almeidas-tatiane <almeidas.tatiane@gmail.com>
2024-01-26 10:54:57 -0300

    Adding CrocoliesSimulation file

--- src/gatling/scala/crocodiles/simulations/CrocodilesSimulation.scala
+++ src/gatling/scala/crocodiles/simulations/CrocodilesSimulation.scala
@@ -1,5 +1,21 @@
 package crocodiles.simulations
 
-class CrocodilesSimulation {
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import crocodiles.objects.Crocodiles
+import scala.concurrent.duration.DurationInt
+
+class CrocodilesSimulation extends Simulation {
+
+  val httpConfiguration = http
+    .acceptHeader("application/json")
+
+  def scnGetAllCrocodiles = scenario("Get All Crocodiles")
+    .repeat(5) {
+      exec(Crocodiles.getAllCrocodiles)
+    }
+
+  setUp(scnGetAllCrocodiles.inject(
+    atOnceUsers(1))).protocols(httpConfiguration)
 
 }
