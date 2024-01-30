58a31b3f9af08756f26d9a9bba7776ef4aebfaae
almeidas-tatiane <almeidas.tatiane@gmail.com>
2024-01-30 16:04:15 -0300

    Adding some checks

--- src/gatling/scala/gatlingdemostore/DemostoreSimulation.scala
+++ src/gatling/scala/gatlingdemostore/DemostoreSimulation.scala
@@ -13,51 +13,88 @@ class DemostoreSimulation extends Simulation {
   val httpProtocol = http
     .baseUrl("https://" + domain)
 
+  object CsmPages {
+    def homePage = {
+      exec(
+        http("Home Page")
+          .get("/")
+          .check(status.is(200))
+          .check(regex("<title>Gatling Demo-Store</title>").exists)
+          .check(css("#_csrf", "content").saveAs("csrfValue"))
+      )
+    }
+
+    def aboutUs = {
+      exec(
+        http("About Us Page")
+          .get("/about-us")
+          .check(status.is(200))
+          .check(substring("About Us"))
+      )
+    }
+
+    def categoriesPage = {
+      exec(
+        http("Categories Page")
+          .get("/category/all")
+      )
+    }
+
+    def loadProductPage = {
+      exec(
+        http("Load Product Page")
+          .get("/product/black-and-red-glasses")
+      )
+    }
+
+    def addProductToCart ={
+      exec(
+        http("Add Product to Cart")
+          .get("/cart/add/19")
+      )
+    }
+
+    def viewCart = {
+      exec(
+        http("View Cart")
+          .get("/cart/view")
+      )
+    }
+
+    def loginPage = {
+      exec(
+        http("Login Page")
+          .post("/login")
+          .formParam("_csrf", "${csrfValue}")
+          .formParam("username", "admin")
+          .formParam("password", "admin")
+      )
+    }
+
+    def checkout = {
+      exec(
+        http("Checkout")
+          .get("/cart/checkout")
+      )
+    }
+  }
+
   val scn = scenario("DemostoreSimulation")
-    .exec(
-      http("Home Page")
-        .get("/")
-        .check(regex("<title>Gatling Demo-Store</title>").exists)
-        .check(css("#_csrf", "content").saveAs("csrfValue"))
-    )
+    .exec(CsmPages.homePage)
     .pause(2)
-    .exec(
-      http("About Us Page")
-        .get("/about-us")
-    )
+    .exec(CsmPages.aboutUs)
     .pause(2)
-    .exec(
-      http("Categories Page")
-        .get("/category/all")
-    )
+    .exec(CsmPages.categoriesPage)
     .pause(2)
-    .exec(
-      http("Load Product Page")
-        .get("/product/black-and-red-glasses")
-     )
+    .exec(CsmPages.loadProductPage)
     .pause(2)
-    .exec(
-      http("Add Product to Cart")
-        .get("/cart/add/19")
-    )
+    .exec(CsmPages.addProductToCart)
     .pause(2)
-    .exec(
-      http("View Cart")
-        .get("/cart/view")
-    )
+    .exec(CsmPages.viewCart)
     .pause(2)
-    .exec(
-      http("Login Page")
-        .post("/login")
-        .formParam("_csrf", "${csrfValue}")
-        .formParam("username", "admin")
-        .formParam("password", "admin")
-    )
+    .exec(CsmPages.loginPage)
     .pause(2)
-    .exec(
-      http("Checkout")
-        .get("/cart/checkout")
-    )
+    .exec(CsmPages.checkout)
 
 	setUp(scn.inject(atOnceUsers(1))).protocols(httpProtocol)
 }
